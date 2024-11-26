90e87830d55c97e8c95abf395f63c8f3b1277dbb
R1mus <142688171+Dilan1x@users.noreply.github.com>
2024-11-26 01:28:24 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Tu Nombre'
-description 'Rich Presence para FiveM'
-version '1.0.0'
+author 'R1mus'
+description 'Rich Presence personalizado para el servidor de roleplay.'
+version '1.1.0'
 
-client_script 'client.lua' -- Nombre del archivo que contiene el script cliente
-
-dependencies {
-    'discord'
+client_scripts {
+    'client.lua'
 }
